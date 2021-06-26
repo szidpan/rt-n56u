fa9944fcc91dcdebf572b784259b77399691a1c7
szidpan <86174307+szidpan@users.noreply.github.com>
2021-06-26 21:24:04 +0800

    Update board.mk

--- trunk/user/shared/board.mk
+++ trunk/user/shared/board.mk
@@ -4,5 +4,5 @@
 # RT-N56U   # ASUS RT-N56U     # RT3883  #
 ##################################################################
 
-CFLAGS += -DBOARD_N56U -DVENDOR_ASUS
-BOARD_NUM_USB_PORTS=2
+CFLAGS += -DBOARD_NEWIFI-MINI -DVENDOR_LENOVO
+BOARD_NUM_USB_PORTS=1
